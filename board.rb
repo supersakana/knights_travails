5fe603b740d62b27b31d33e2d3839af8e577778f
supersakana <zacwilliamson@icloud.com>
2022-04-21 14:42:23 -0600

    added basic foundation and notes in the README

---
+++ board.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+# 8 x 8 cell game board
+class Board
+  def initialize
+    # code to run
+  end
+
+  def print_board
+    # prints the game board
+  end
+end
---
+++ cell.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+# Each cell has a pointer: (n = north, s = south, e = east, w = west)
+# node also contains a pointer
+class Cell
+  def initialize
+    # code to run
+  end
+end
